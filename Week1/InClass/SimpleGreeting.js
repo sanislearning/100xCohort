3a472db32cf2612676ac1bc4d69f0a8f7238128f
sanislearning <sanjaybiju10@gmail.com>
2025-06-08 23:30:17 +0530

    Added a few more exercises

---
+++ Week1/InClass/SimpleGreeting.js
@@ -0,0 +1,11 @@
+let Gender='male';
+if (Gender==='male'){
+    console.log("Hello, Sir!");
+}
+else if (Gender==='female'){
+    console.log("Hello, Madam!");
+}
+
+else {
+    console.log("Hello, there!");
+}
