744628ce7824186f1aebd8618d0d210d03553d79
sanislearning <sanjaybiju10@gmail.com>
2025-06-11 23:47:36 +0530

    Doing the assignments

---
+++ Week1/Assignments/TimeCalculation.js
@@ -0,0 +1,8 @@
+//Calculate the time it takes between a setTimeout call and the inner function actually running
+let startTime=Date.now(); //Date.now() gets time at the instant
+function timecalc(){
+    const endTime=Date.now();
+    const elapsedTime=endTime-startTime;
+    console.log(`Elaspsed time : ${elapsedTime} ms`);
+}
+setTimeout(timecalc,1000);
---
+++ Week1/Assignments/counter.js
@@ -0,0 +1,5 @@
+// Create a counter in JavaScript (counts from 30 to 0)
+let counter=30;
+for (let i=counter;i>=0;i--){
+    console.log(i)
+}
