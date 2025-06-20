25e7b31bfc4f80bd3f719c3c9081e41f323995fc
sanislearning <sanjaybiju10@gmail.com>
2025-06-20 16:23:56 +0530

    Made the calculator better

--- InPerson/Day1/simpleCalc/calc.js
+++ InPerson/Day1/simpleCalc/calc.js
@@ -1,29 +1,8 @@
-function addition(){
+function sol(arg){
     //alert("Addition function called");
     let val1=document.getElementById("first").value;
     let val2=document.getElementById("two").value;
-    let val=parseInt(val1)+parseInt(val2);
-    document.getElementById('answer').textContent=val;
-
-}
-function subtract(){
-    //alert("Subtraction function called");
-    let val1=document.getElementById("first").value;
-    let val2=document.getElementById("two").value;
-    let val=parseInt(val1)-parseInt(val2);
+    let val=eval(val1+arg+val2)
     document.getElementById('answer').textContent=val;
 }
-function multiply(){
-    //alert("Multiplication function called");
-    let val1=document.getElementById('first').value;
-    let val2=document.getElementById("two").value;
-    let val=parseInt(val1)*parseInt(val2);
-    document.getElementById('answer').textContent=val;
-}
-function divide(){
-    //alert("Divide function called");
-    let val1=document.getElementById('first').value;
-    let val2=document.getElementById('two').value;
-    let val=parseFloat(val1)/parseFloat(val2);
-    document.getElementById('answer').textContent=val;
-}
+//Eval isn't like a reccomended function to run since it will evaluate anything you give it
