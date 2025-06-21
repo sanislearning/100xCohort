e96272f24c5c00f4b10a610cd6594b0eccde3c90
sanislearning <sanjaybiju10@gmail.com>
2025-06-21 15:34:59 +0530

    Added things that are currently untracked

--- InPerson/Day2/NameSorter/nametaker.js
+++ InPerson/Day2/NameSorter/nametaker.js
@@ -1,14 +1,26 @@
 const axios=require("axios")
-
+const fs=require('fs');
 function NameTaker(){
-    const res = axios.get("https://raw.githubusercontent.com/hkirat/names/refs/heads/main/a.json") //input the url inside this
+    return axios.get("https://raw.githubusercontent.com/hkirat/names/refs/heads/main/a.json") //input the url inside this
     //you get back a response object from the url
-        return res.then(response=>response.data) //extracts resposnse from the https response
-        .then(data =>console.log(data)) // logs the actual data
-        .catch(error=>{
-            console.error(error);
-        });
-
+    .then(response=>{
+        return response.data; //we return the data here
+    })
 }
+
 NameTaker().then(val=>{
-    console.log('final val:,val')})
+    console.log("Final Value:",val);
+    ans=[];
+    ansb=[];
+    for(let i=0;i<val.names.length;i++){
+    if (val.names[i][0]=='A'){
+        ans.push(val.names[i]);
+    }
+    else if(val.names[i][0]=='B'){
+        ansb.push(val.names[i]);
+    }
+    }
+    fs.writeFile('r.txt',ans.toString(),'utf-8',function(){})
+    fs.writeFile('h.txt',ansb.toString(),'utf-8',function(){})
+})
+
